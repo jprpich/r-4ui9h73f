3cb36e05b56666e87ac4f5495bf06003bda161b5
Joshua <joshuaprpich@gmail.com>
2018-04-19 08:38:19 -0500

    resolve challenge

--- src/App.js
+++ src/App.js
@@ -2,14 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.changeContent.bind(this)} />
+        <p className="repeater">{this.state.pContent}</p>
       </div>
     );
+  }
+
+  changeContent(e){
+    this.setState({
+      pContent: e.target.value
+    })
   }
 }
 
